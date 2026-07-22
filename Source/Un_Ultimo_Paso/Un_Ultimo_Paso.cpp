3e7690d686f0c57db97398cc7725993ffa2f8838
Admonsis <Admonsis@ISC417>
2026-07-22 14:11:30 -0500

    Primer Commit Configuracion del proyecto

---
+++ Source/Un_Ultimo_Paso/Un_Ultimo_Paso.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Un_Ultimo_Paso.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Un_Ultimo_Paso, "Un_Ultimo_Paso" );
