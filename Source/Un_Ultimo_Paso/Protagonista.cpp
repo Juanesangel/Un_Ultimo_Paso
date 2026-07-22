93e47ea4667544a687f8db2948b3b62035098a55
Admonsis <Admonsis@ISC417>
2026-07-22 15:51:12 -0500

    Mecanicas Movimiento Personaje

---
+++ Source/Un_Ultimo_Paso/Protagonista.cpp
@@ -0,0 +1,150 @@
+#include "Protagonista.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AProtagonista::AProtagonista()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// --- Cámara ---
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+
+	// --- Push detector ---
+	PushDetector = CreateDefaultSubobject<UBoxComponent>(TEXT("PushDetector"));
+	PushDetector->SetupAttachment(RootComponent);
+	PushDetector->SetBoxExtent(FVector(50.f, 50.f, 50.f));
+	PushDetector->SetRelativeLocation(FVector(60.f, 0.f, 0.f));
+	PushDetector->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	PushDetector->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+}
+
+// Called when the game starts or when spawned
+void AProtagonista::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PushDetector->OnComponentBeginOverlap.AddDynamic(this, &AProtagonista::OnPushDetectorBeginOverlap);
+	PushDetector->OnComponentEndOverlap.AddDynamic(this, &AProtagonista::OnPushDetectorEndOverlap);
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			if (DefaultMappingContext)
+			{
+				Subsystem->AddMappingContext(DefaultMappingContext, 0);
+			}
+		}
+	}
+}
+
+// Called every frame
+void AProtagonista::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsPushing && CurrentPushable)
+	{
+		const FVector PushDirection = GetActorForwardVector();
+		CurrentPushable->AddForce(PushDirection * PushForce, NAME_None, true);
+	}
+}
+
+// Called to bind functionality to input
+void AProtagonista::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_Move)
+			EnhancedInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AProtagonista::OnMove);
+
+		if (IA_Look)
+			EnhancedInput->BindAction(IA_Look, ETriggerEvent::Triggered, this, &AProtagonista::OnLook);
+
+		if (IA_Jump)
+		{
+			EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Started, this, &ACharacter::Jump);
+			EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		}
+
+		if (IA_Empujar)
+		{
+			EnhancedInput->BindAction(IA_Empujar, ETriggerEvent::Started, this, &AProtagonista::OnEmpujarStarted);
+			EnhancedInput->BindAction(IA_Empujar, ETriggerEvent::Completed, this, &AProtagonista::OnEmpujarCompleted);
+			EnhancedInput->BindAction(IA_Empujar, ETriggerEvent::Canceled, this, &AProtagonista::OnEmpujarCompleted);
+		}
+	}
+}
+
+void AProtagonista::OnMove(const FInputActionValue& Value)
+{
+	const FVector2D MoveInput = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		const FRotator ControlRotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+		const FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDir, MoveInput.Y);
+		AddMovementInput(RightDir, MoveInput.X);
+	}
+}
+
+void AProtagonista::OnLook(const FInputActionValue& Value)
+{
+	const FVector2D LookInput = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookInput.X);
+	AddControllerPitchInput(LookInput.Y);
+}
+
+void AProtagonista::OnEmpujarStarted(const FInputActionValue& Value)
+{
+	bIsPushing = true;
+}
+
+void AProtagonista::OnEmpujarCompleted(const FInputActionValue& Value)
+{
+	bIsPushing = false;
+}
+
+void AProtagonista::OnPushDetectorBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherComp && OtherComp->IsSimulatingPhysics())
+	{
+		CurrentPushable = OtherComp;
+	}
+}
+
+void AProtagonista::OnPushDetectorEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherComp == CurrentPushable)
+	{
+		CurrentPushable = nullptr;
+	}
+}
---
+++ Source/Un_Ultimo_Paso/Protagonista.h
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "Protagonista.generated.h"
+
+class UBoxComponent;
+class UInputAction;
+class UInputMappingContext;
+class UPrimitiveComponent;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class UN_ULTIMO_PASO_API AProtagonista : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AProtagonista();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// --- Cámara ---
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	TObjectPtr<UCameraComponent> Camera;
+
+	// --- Enhanced Input ---
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> IA_Move;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> IA_Look;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> IA_Jump;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Push")
+	TObjectPtr<UInputAction> IA_Empujar;
+
+	// --- Mecánica de empuje ---
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Push")
+	TObjectPtr<UBoxComponent> PushDetector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Push")
+	float PushForce = 800.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Push")
+	TObjectPtr<UPrimitiveComponent> CurrentPushable;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Push")
+	bool bIsPushing = false;
+
+	// --- Handlers de movimiento ---
+	void OnMove(const FInputActionValue& Value);
+	void OnLook(const FInputActionValue& Value);
+
+	// --- Handlers de empuje ---
+	void OnEmpujarStarted(const FInputActionValue& Value);
+	void OnEmpujarCompleted(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void OnPushDetectorBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnPushDetectorEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
