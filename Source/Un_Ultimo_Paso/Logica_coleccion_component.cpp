1c4863993dedb6522b992e6cfe4cd2bc4c980f96
Admonsis <Admonsis@ISC417>
2026-07-22 17:01:20 -0500

    Primer cambio para mecanica encontrar objetos

---
+++ Source/Un_Ultimo_Paso/Logica_coleccion_component.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Logica_coleccion_component.h"
+
+// Sets default values
+ALogica_coleccion_component::ALogica_coleccion_component()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALogica_coleccion_component::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALogica_coleccion_component::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
